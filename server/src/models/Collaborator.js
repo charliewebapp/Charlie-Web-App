d5d0256ea2925c12f67b9fe8c052dbd555e74625
Brian Paiba <brianmanuelpaiba@outlook.es>
2024-02-06 14:50:43 -0300

    ADD models client

---
+++ server/src/models/Collaborator.js
@@ -0,0 +1,33 @@
+const { DataTypes } = require('sequelize');
+module.exports = (sequelize) => {
+
+  sequelize.define('Collaborator', {
+    id:{
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    mail:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    status:{
+      type: DataTypes.ENUM('active', 'inactive'),
+      defaultValue: 'active',
+      allowNull: false,
+    }
+  },
+  {
+    timestamps: false
+  }
+  );
+};
---
+++ server/src/models/Product.js
@@ -0,0 +1,43 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define(
+    "Product",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      brand: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      price: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+      },
+      stock: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true,
+        allowNull: false,
+      },
+    },
+    {
+      timestamps: false,
+    }
+  );
+};
