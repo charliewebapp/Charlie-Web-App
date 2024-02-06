a6ec4464c418b659601806bb44a198b3fe9ed97e
Luciano Moren <lucianomorenandre03@gmail.com>
2024-02-06 13:42:19 -0300

    Boiler Front

---
+++ client/src/views/Admin/FormPostProduct/FormPostProduct.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import style from "./formpostproduct.module.css";
+
+function FormPostProduct() {
+  return <div>FormPostProduct</div>;
+}
+
+export default FormPostProduct;
