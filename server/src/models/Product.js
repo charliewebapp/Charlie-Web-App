2c91fb824efd3208e23704bad52f4e72a738f572
Santino <schielsantino@gmail.com>
2024-02-06 16:42:48 -0300

    ADD modelos-Purchase-PurchaseHistory

--- server/src/models/Product.js
+++ server/src/models/Product.js
@@ -27,7 +27,7 @@ module.exports = (sequelize) => {
         allowNull: false,
       },
       price: {
-        type: DataTypes.FLOAT,
+        type: DataTypes.INTEGER,
         allowNull: false,
       },
       stock: {
