0bb73e0b01bedee98a790d0b0a726b317b66084b
Nicolas <comercialfecor@gmail.com>
2024-02-06 16:34:56 -0500

    routePostClient

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -3,13 +3,17 @@ const { Router } = require("express");
 const router = Router();
 
 //products
-router.get("/product", getProductHandler )
-router.get("/product/:category", getProductCategoryHandler )
-router.post("/product",postProductHandler )
-router.put("/product", putProductHandler )
-router.delete("/product", deleteProductHandler)
+router.get("/product", getProductHandler);
+router.get("/product/:category", getProductCategoryHandler);
+router.post("/product", postProductHandler);
+router.put("/product", putProductHandler);
+router.delete("/product", deleteProductHandler);
+
+//categories
+
+//clients
+router.post("/client", postClientHandler);
 
 //admins
-
 
 module.exports = router;
