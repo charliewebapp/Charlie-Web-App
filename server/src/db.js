b1ea80f8b746ffba568540a58011ce69e6066270
Brian Paiba <brianmanuelpaiba@outlook.es>
2024-02-06 09:40:34 -0300

    ADD database

--- server/src/db.js
+++ server/src/db.js
@@ -1,46 +1,49 @@
-// require("dotenv").config();
-// const { Sequelize } = require("sequelize");
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
 
-// const fs = require("fs");
-// const path = require("path");
-// const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const fs = require("fs");
+const path = require("path");
+const { DB_RENDER } = process.env;
 
-// const sequelize = new Sequelize(
-//   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`,
-//   {
-//     logging: false,
-//     native: false,
-//   }
-// );
+const sequelize = new Sequelize(DB_RENDER, {
+    logging: false,
+    native: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  });
 
-// const basename = path.basename(__filename);
+const basename = path.basename(__filename);
 
-// const modelDefiners = [];
+const modelDefiners = [];
 
-// fs.readdirSync(path.join(__dirname, "/models"))
-//   .filter(
-//     (file) =>
-//       file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
-//   )
-//   .forEach((file) => {
-//     modelDefiners.push(require(path.join(__dirname, "/models", file)));
-//   });
+fs.readdirSync(path.join(__dirname, "/models"))
+  .filter(
+    (file) =>
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+  )
+  .forEach((file) => {
+    modelDefiners.push(require(path.join(__dirname, "/models", file)));
+  });
 
-// modelDefiners.forEach((model) => model(sequelize));
+modelDefiners.forEach((model) => model(sequelize));
 
-// let entries = Object.entries(sequelize.models);
-// let capsEntries = entries.map((entry) => [
-//   entry[0][0].toUpperCase() + entry[0].slice(1),
-//   entry[1],
-// ]);
-// sequelize.models = Object.fromEntries(capsEntries);
+let entries = Object.entries(sequelize.models);
+let capsEntries = entries.map((entry) => [
+  entry[0][0].toUpperCase() + entry[0].slice(1),
+  entry[1],
+]);
+sequelize.models = Object.fromEntries(capsEntries);
 
-// const { Country } = sequelize.models;
+const { Country } = sequelize.models;
 
-// // Aca vendrian las relaciones
-// // Product.hasMany(Reviews);
+// Aca vendrian las relaciones
+// Product.hasMany(Reviews);
 
-// module.exports = {
-//   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
-//   conn: sequelize, // para importart la conexión { conn } = require('./db.js');
-// };
+module.exports = {
+  ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
+  conn: sequelize, // para importart la conexión { conn } = require('./db.js');
+};
