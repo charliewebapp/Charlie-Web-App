fed6b0575e53849198c38f8f1fc28d29ba253a07
Nicolas <comercialfecor@gmail.com>
2024-02-06 17:57:45 -0500

    controllerPostClient

---
+++ server/src/controllers/postClients.js
@@ -0,0 +1,7 @@
+const { Client } = require("../db");
+
+const postClients = async function (data) {
+  const newClient = await Client.create(data);
+  return newClient;
+};
+module.exports = postClients;
