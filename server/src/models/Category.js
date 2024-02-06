d9ca9b963331440a4aba84c732539f375f3a6f6a
Brian Paiba <brianmanuelpaiba@outlook.es>
2024-02-06 18:06:04 -0300

    ADD routes products

--- server/src/models/Category.js
+++ server/src/models/Category.js
@@ -1,19 +1,29 @@
 const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-  sequelize.define("Category", {
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      allowNull: false,
-      primaryKey: true,
+  sequelize.define(
+    "Category",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.ENUM(
+          "Tragos",
+          "Cervezas",
+          "Botellas",
+          "Vinos",
+          "Shots",
+          "Sin Alcohol"
+        ),
+        allowNull: false,
+      },
     },
-    name:{
-      type: DataTypes.STRING,
-      allowNull: false,
+    {
+      timestamps: false,
     }
-  },
-  {
-    timestamps: false
-  });
+  );
 };
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,6 +2,14 @@ const { Router } = require("express");
 
 const router = Router();
 
+//products
+router.get("/product", getProductHandler )
+router.get("/product/:category", getProductCategoryHandler )
+router.post("/product",postProductHandler )
+router.put("/product", putProductHandler )
+router.delete("/product", deleteProductHandler)
+
+//admins
 
 
 module.exports = router;
