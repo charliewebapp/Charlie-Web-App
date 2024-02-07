cd61b9b855151634d14d8132f3fb69973fed4dbf
Natalia Andrea Lossada <natilossada@hotmail.com>
2024-02-07 19:30:15 -0300

    ADD formupdate formpost products admin redux actions

--- client/src/views/Users/Profile/Profile.jsx
+++ client/src/views/Users/Profile/Profile.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import style from "./profile.module.css";
 
 function Profile() {
-  return <div>Profile</div>;
+  return (
+    <div>
+      <p> MERCADO PAGO PARA TEST DE BRIAN</p>
+      <button>TEST</button>
+    </div>
+  )
+
 }
 
 export default Profile;
