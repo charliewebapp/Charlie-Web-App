78885f1ecb0dfefee9c41711d24349d86ccc25a5
ErikaFogar <erikafogar.j@gmail.com>
2024-02-07 13:47:27 +0100

    dashboardAdmin

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,79 +1,68 @@
+import "./App.css";
+import { Routes, Route, useLocation } from "react-router-dom";
+import FormUpdateProductAdmin from "./views/Admin/FormUpdateProductAdmin/FormUpdateProductAdmin";
+import DashboardAdmin from "./views/Admin/DashboardAdmin/DashboardAdmin";
+import FormPostEmployee from "./views/Admin/FormPostEmployee/FormPostEmployee";
+import FormClubProfile from "./views/Admin/FormClubProfile/FormClubProfile";
+import LandingAdmin from "./views/Admin/LandingAdmin/LandingAdmin";
+import FormPostProductAdmin from "./views/Admin/FormPostProductAdmin/FormPostProductAdmin";
+import FormUpdateEmployee from "./views/Admin/FormUpdateEmployee/FormUpdateEmployee";
 
-import './App.css'
-import { Routes, Route, useLocation } from 'react-router-dom'
-import FormUpdateProductAdmin from './views/Admin/FormUpdateProductAdmin/FormUpdateProductAdmin';
-import DashboardAdmin from './views/Admin/DashboardAdmin/DashboardAdmin';
-import FormPostEmployee from './views/Admin/FormPostEmployee/FormPostEmployee';
-import FormClubProfile from './views/Admin/FormClubProfile/FormClubProfile';
-import LandingAdmin from './views/Admin/LandingAdmin/LandingAdmin';
-import FormPostProductAdmin from './views/Admin/FormPostProductAdmin/FormPostProductAdmin';
-import FormUpdateEmployee from './views/Admin/FormUpdateEmployee/FormUpdateEmployee';
-
-import DashboardSuperA from './views/SuperAdmin/DashboardSuperA/DashboardSuperA';
-import FormPostClubSuperA from './views/SuperAdmin/FormPostClubSuperA/FormPostClubSuperA';
-
-
-
+import DashboardSuperA from "./views/SuperAdmin/DashboardSuperA/DashboardSuperA";
+import FormPostClubSuperA from "./views/SuperAdmin/FormPostClubSuperA/FormPostClubSuperA";
 
 const App = () => {
-
   return (
     <>
       <Routes>
         {/* //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! RUTAS SUPERADMIN - agregar SuperA a cada component */}
         <Route
-          path='/superadmin'
-          element={<LandingSuperA />} // aca va form login
+          path="/superadmin"
+          // element={<LandingSuperA />} // aca va form login //Descomentar al crear
         />
 
         <Route
-          path='/superadmin/dashboard'
+          path="/superadmin/dashboard"
           element={<DashboardSuperA />} // aca va form login
         />
 
         <Route
-          path='/superadmin/addclub'
+          path="/superadmin/addclub"
           element={<FormPostClubSuperA />} // AGREGA BOLICHE -> designar 1 solo usuario y contraseña de administrador
         />
 
-
-
         {/* //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! RUTAS DEL ADMINISTRADOR */}
-        <Route
-          path='/admin'
-          element={<LandingAdmin />}
-        />
+        <Route path="/admin" element={<LandingAdmin />} />
 
         {/* HACER UN GET AL BACK PARA NOMBRE DE BOLICHE */}
         <Route
-          path='/admin/:clubName/dashbordAdmin'
+          path="/admin/:clubName/dashbordAdmin"
           element={<DashboardAdmin />} //aca mismo lista VENTAS solo mostrar /PRODUCTOS (CRUD) / COLABORADORES(crea edita elimina)
         />
 
         <Route
-          path='/admin/:clubName/addproduct'
+          path="/admin/:clubName/addproduct"
           element={<FormPostProductAdmin />}
         />
         <Route
-          path='/admin/:clubName/editproduct'
+          path="/admin/:clubName/editproduct"
           element={<FormUpdateProductAdmin />}
         />
 
         <Route
-          path='/admin/:clubName/addemployee'
-          element={<FormPostEmployee />}  //agrega mas administradores o colaboradores
+          path="/admin/:clubName/addemployee"
+          element={<FormPostEmployee />} //agrega mas administradores o colaboradores
         />
 
         <Route
-          path='/admin/:clubName/updateemployee'
+          path="/admin/:clubName/updateemployee"
           element={<FormUpdateEmployee />}
         />
 
         <Route
-          path='/admin/:clubName/clubprofile'
+          path="/admin/:clubName/clubprofile"
           element={<FormClubProfile />}
         />
-
 
         {/*//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! RUTAS USUARIOS -> los componentes no dicen user*/}
         {/* FALTAN IMPORTAR
@@ -88,10 +77,9 @@ const App = () => {
         ...FALTAN DEMAS RUTAS
         
         */}
-
       </Routes>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
