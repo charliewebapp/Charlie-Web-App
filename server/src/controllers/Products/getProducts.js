60f534b48ff5c991b659270bca53c35ba8dcc18c
Brian Paiba <brianmanuelpaiba@outlook.es>
2024-02-06 23:43:28 -0300

    ADD Controllers II

--- server/src/controllers/Products/getProducts.js
+++ server/src/controllers/Products/getProducts.js
@@ -1,10 +1,8 @@
 const { Product } = require("../../db");
 
 const getProducts = async () => {
-console.log('ejecutando...');
-  const allProductos = await Product.findAll();
-  console.log(allProductos);
-  return allProductos;
+  const allProducts = await Product.findAll();
+  return allProducts;
 };
 
 module.exports = getProducts;
