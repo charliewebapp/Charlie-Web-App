316e318d1f24395f04d00c60f57f267d67d3f6d5
ErikaFogar <erikafogar.j@gmail.com>
2024-02-07 13:47:27 +0100

    dashboardAdmin

---
+++ client/src/redux/reducer.js
@@ -0,0 +1,10 @@
+const initialState = {};
+
+export default function reducer(state = initialState, { type, payload }) {
+  //* action = { type:.., payload:...}
+  // const { type, payload } = action;
+  switch (type) {
+    default:
+      return { ...state };
+  }
+}
