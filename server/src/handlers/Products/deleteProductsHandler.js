8c15cdba1bb90e8f265decf865d0e5b09cdd9589
ErikaFogar <erikafogar.j@gmail.com>
2024-02-08 22:17:34 +0100

    ADMIN CRUD STOCK

--- server/src/handlers/Products/deleteProductsHandler.js
+++ server/src/handlers/Products/deleteProductsHandler.js
@@ -1,15 +1,14 @@
-const deleteProducts = require('../../controllers/Products/deleteProducts');
+const deleteProducts = require("../../controllers/Products/deleteProducts");
 
-const deleteProductHandler = async (req,res) => {
-    try {
-        const {client, product} = req.params
-        const clientMinus = client.toLowerCase()
-        const productMinus = product.toLowerCase()
-        const deletedProduct =await deleteProducts(clientMinus,productMinus)
-        return res.status(201).json(deletedProduct)
-    } catch (error) {
-        return res.status(500).json({error:error.message})
-    }
-}
+const deleteProductHandler = async (req, res) => {
+  try {
+    const { client, product } = req.params;
+    const clientMinus = client.toLowerCase();
+    const deletedProduct = await deleteProducts(clientMinus, product);
+    return res.status(201).json(deletedProduct);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
 
-module.exports = deleteProductHandler
+module.exports = deleteProductHandler;
