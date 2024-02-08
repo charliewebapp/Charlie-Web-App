622fcd525dbaa886cc1d6129f38fad115c8d5257
ErikaFogar <erikafogar.j@gmail.com>
2024-02-08 22:13:45 +0100

    Admin Stock CRUD

--- client/src/redux/actions-types.js
+++ client/src/redux/actions-types.js
@@ -2,3 +2,4 @@ export const GET_PRODUCTS = "GET_PRODUCTS";
 export const POST_PRODUCT = "POST_PRODUCT";
 export const UPDATE_PRODUCT = "UPDATE_PRODUCT";
 export const POST_BOLICHE = "POST_BOLICHES";
+export const DELETE_PRODUCT = "DELETE_PRODUCT";
