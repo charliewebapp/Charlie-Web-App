42e5abe370099644ed0b247a8d7580b388181064
Natalia Andrea Lossada <natilossada@hotmail.com>
2024-02-09 11:12:28 -0300

    MOD CONECTION ADMIN DASHBOARD AND FORMS

--- client/src/redux/actions-types.js
+++ client/src/redux/actions-types.js
@@ -1,7 +1,16 @@
+//! /////////////// PRODUCTS ///////////////////////
 export const GET_PRODUCTS = "GET_PRODUCTS";
 export const POST_PRODUCT = "POST_PRODUCT";
 export const UPDATE_PRODUCT = "UPDATE_PRODUCT";
 export const DELETE_PRODUCT = "DELETE_PRODUCT";
+
+
+//! /////////////// COLLABORATORS ///////////////////////
+export const GET_COLLABORATORS = "GET_COLLABORATORS";
+export const POST_COLLABORATOR = "POST_COLLABORATOR";
+export const UPDATE_COLLABORATOR = "UPDATE_COLLABORATOR";
+export const DELETE_COLLABORATOR = "DELETE_COLLABORATOR";
+
 
 //! /////////////// SUPER ADMIN ///////////////////////
 export const POST_BOLICHE = "POST_BOLICHE";
