812c8a57f8e6537a3a9a697557e398fd0353296a
Natalia Andrea Lossada <natilossada@hotmail.com>
2024-02-09 12:58:43 -0300

    MODRutasdeleteadminSERVER

--- server/src/handlers/Collaborators/deleteCollaboratorHandler.js
+++ server/src/handlers/Collaborators/deleteCollaboratorHandler.js
@@ -4,9 +4,8 @@ const deleteControllerHandler = async (req, res) => {
   try {
     const { client, user } = req.params;
     console.log(client, user);
-    const clientMinus = client.toLowerCase();
     const userMinus = user.toLowerCase();
-    const deletedController = await deleteCollaborator(clientMinus,userMinus);
+    const deletedController = await deleteCollaborator(client, userMinus);
     return res.status(201).json(deletedController);
   } catch (error) {
     return res.status(500).json({ error: error.message });
