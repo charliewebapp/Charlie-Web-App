5b3a8b67652e1033f2ac5c68d52e380dc368ff28
Natalia Andrea Lossada <natilossada@hotmail.com>
2024-02-09 12:58:43 -0300

    MODRutasdeleteadminSERVER

--- server/src/controllers/Collaborators/deleteCollaborator.js
+++ server/src/controllers/Collaborators/deleteCollaborator.js
@@ -1,11 +1,11 @@
-const { Client, Administrator } = require("../../db");
+const { Client, Collaborator } = require("../../db");
 
 const deleteAdmins = async (client, user) => {
   const clientSearched = await Client.findOne({ where: { name: client } });
   if (!clientSearched) throw new Error("El colaborador no existe.");
   const clientId = clientSearched.dataValues.id;
 
-  const deletedProduct = await Administrator.destroy({
+  const deletedProduct = await Collaborator.destroy({
     where: {
       ClientId: clientId,
       id: user,
