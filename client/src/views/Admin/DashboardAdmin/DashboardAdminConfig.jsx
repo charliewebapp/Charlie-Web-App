9bdbc3bdf27059817f586a72d3c2905c024ad15f
Brian Paiba <brianmanuelpaiba@outlook.es>
2024-02-12 17:09:56 -0300

    cambia nombre dasboardadmindsales en dashboardadmin

--- client/src/views/Admin/DashboardAdmin/DashboardAdminConfig.jsx
+++ client/src/views/Admin/DashboardAdmin/DashboardAdminConfig.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import style from "./dashboardAdmin.module.css";
+import { v4 as uuidv4 } from "uuid";
 
 function DashboardAdminConfig() {
+
+  const authorization = () => {
+    const redirectUri =
+      "https://mercadopago-7p1q.onrender.com/mercadopago-authorization/success";
+    const clientId = "7378685924902197";
+    const state = uuidv4();
+    const authorizationUrl = `https://auth.mercadopago.com/authorization?client_id=${clientId}&response_type=code&platform_id=mp&state=${state}&redirect_uri=${redirectUri}`;
+    window.open(authorizationUrl);
+  };
+
   return (
     <div className={style.container}>
       <h2 className={style.h2}>Configuración</h2>
       <div containerButton>
         <div className={style.linkContainer}>
           {/* Dentro del div poner el link to */}
-          <button className={style.buttonConfig}> Conectar MercadoPago</button>
+          <button className={style.buttonConfig} onClick={authorization}>Conectar MercadoPago</button>
         </div>
         <br />
         <div className={style.linkContainer}>
