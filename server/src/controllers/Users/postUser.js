c793a384dd30a72ccaf5b20971e83341dc87fc1e
elopezfilippini <elopezfilippini@gmail.com>
2024-02-16 10:35:05 -0300

    Fixeo PostUser

--- server/src/controllers/Users/postUser.js
+++ server/src/controllers/Users/postUser.js
@@ -1,7 +1,13 @@
-const { User } = require("../../db");
+  const { User } = require("../../db");
 
-const postUser = async function (data) {
-  const newUser = await User.create(data);
-  return newUser;
-};
-module.exports = postUser;
+  const postUser = async function (data) {
+    const [user] = await User.findOrCreate({
+      where: {
+        id: data.id,
+      },
+      defaults: data, 
+    });
+  
+    return user; 
+  };
+  module.exports = postUser;
