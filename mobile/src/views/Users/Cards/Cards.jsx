1fd57d0122bb3479bf1e8ba2f895d60e01d710e3
ErikaFogar <erikafogar.j@gmail.com>
2024-02-18 20:27:13 +0100

    ADD carrousel categorys cards

--- mobile/src/views/Users/Cards/Cards.jsx
+++ mobile/src/views/Users/Cards/Cards.jsx
@@ -1,33 +1,94 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams, Link } from "react-router-dom";
 import { getProducts } from "../../../redux/actions";
 import Card from "../Card/Card";
 import styles from "./Cards.module.css";
 import NavBarUser from "../NavBarUser/NavBarUser";
+import { TiShoppingCart } from "react-icons/ti";
 
 function Cards() {
   const { clubName } = useParams();
   let { category } = useParams();
   const dispatch = useDispatch();
   const allProducts = useSelector((state) => state.allProducts);
+  const cart = useSelector((state) => state.cart);
 
   useEffect(() => {
     dispatch(getProducts(clubName));
-  }, []);
-  //   console.log("productos disponibles", allProducts);
+  }, [clubName, dispatch]);
 
-  // if categoria sin alcohol - falta espacio en el medio
   if (category === "sinalcohol") {
-    category = "Sin Alcohol"
+    category = "Sin Alcohol";
   }
+
   const filteredProducts = allProducts.filter(
     (product) => product.category.toLowerCase() === category.toLowerCase()
   );
 
+  // Lógica para mostrar solo tres botones a la vez
+  const categories = [
+    "Tragos", //0
+    "Cervezas", //1
+    "Botellas", //2
+    "Vinos", //3
+    "Shots", //4
+    "Sin Alcohol", //5
+  ];
+
+  // Encontrar el índice de la categoría actual
+  const currentIndex = categories.indexOf(
+    category.charAt(0).toUpperCase() + category.slice(1)
+  );
+
+  // Determinar las categorías previa, actual y siguiente
+  let prevCategoryIndex =
+    (currentIndex - 1 + categories.length) % categories.length;
+  let nextCategoryIndex = (currentIndex + 1) % categories.length;
+
+  // Ajustar para el ciclo circular
+  if (prevCategoryIndex === -1) {
+    prevCategoryIndex = categories.length - 1;
+  }
+
+  if (nextCategoryIndex === categories.length) {
+    nextCategoryIndex = 0;
+  }
+
+  // Calcular el total de elementos en el carrito
+  const totalItemsInCart = cart.reduce((acc, item) => acc + item.quantity, 0);
+
   return (
     <div className={styles.container}>
       <NavBarUser />
+      <div className={styles.carrousel}>
+        <Link
+          to={`/${clubName}/cards/${categories[prevCategoryIndex]
+            .toLowerCase()
+            .replace(/\s+/g, "")}`}
+          className={styles.button}
+        >
+          {categories[prevCategoryIndex]}
+        </Link>
+        <span className={styles.span}>|</span>
+        <Link
+          to={`/${clubName}/cards/${categories[currentIndex]
+            .toLowerCase()
+            .replace(/\s+/g, "")}`}
+          className={`${styles.button} ${styles.active}`}
+        >
+          {categories[currentIndex]}
+        </Link>
+        <span className={styles.span}>|</span>
+        <Link
+          to={`/${clubName}/cards/${categories[nextCategoryIndex]
+            .toLowerCase()
+            .replace(/\s+/g, "")}`}
+          className={styles.button}
+        >
+          {categories[nextCategoryIndex]}
+        </Link>
+      </div>
       {filteredProducts.map((product, index) => (
         <Card
           key={index}
@@ -38,9 +99,10 @@ function Cards() {
           description={product.description}
         />
       ))}
-      <div>
-        <Link to={`/${clubName}/cart`}>
-          <button>Carrito</button>
+      <div className={styles.cart}>
+        <Link to={`/${clubName}/cart`} className={styles.cartLink}>
+          <TiShoppingCart size={24} />
+          <span className={styles.cartItemCount}>{totalItemsInCart}</span>
         </Link>
       </div>
     </div>
