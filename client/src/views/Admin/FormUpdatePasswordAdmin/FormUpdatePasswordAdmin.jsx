36b45e3ba0eb7c0c40471a8a0df3ba04eab1dc0b
Luciano Moren <lucianomorenandre03@gmail.com>
2024-02-19 14:33:48 -0300

    Changes in dashboard administrator

--- client/src/views/Admin/FormUpdatePasswordAdmin/FormUpdatePasswordAdmin.jsx
+++ client/src/views/Admin/FormUpdatePasswordAdmin/FormUpdatePasswordAdmin.jsx
@@ -2,10 +2,10 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import style from "./FormUpdatePasswordAdmin.module.css";
+import style from "../../SuperAdmin/DashboardSuperA/dashboard.module.css";
 import { useParams } from "react-router-dom";
 import { updateAdmin } from "../../../redux/actions";
-
+import Swal from "sweetalert2";
 function FormUpdatePasswordAdmin() {
   const dispatch = useDispatch();
   const selectAdminID = useSelector((state) => state.selectAdminLogin);
@@ -75,6 +75,31 @@ function FormUpdatePasswordAdmin() {
   const formData = {
     password: newPassword,
   };
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+
+  //   if (currentPasswordInput !== currentPassword) {
+  //     setCurrentPasswordError("La contraseña actual es incorrecta.");
+  //   }
+
+  //   if (newPassword !== confirmNewPassword) {
+  //     setNewPasswordError("Las contraseñas nuevas no coinciden.");
+  //   }
+
+  //   if (
+  //     currentPasswordInput === currentPassword &&
+  //     newPassword === confirmNewPassword
+  //   ) {
+  //     //! Agregar sweet alert
+  //     dispatch(updateAdmin(formData, adminID, clubName));
+  //     setCurrentPasswordInput("");
+  //     setNewPassword("");
+  //     setConfirmNewPassword("");
+  //     setCurrentPasswordError("");
+  //     setNewPasswordError("");
+  //     setConfirmNewPasswordError("");
+  //   }
+  // };
   const handleSubmit = (e) => {
     e.preventDefault();
 
@@ -90,23 +115,41 @@ function FormUpdatePasswordAdmin() {
       currentPasswordInput === currentPassword &&
       newPassword === confirmNewPassword
     ) {
-      //! Agregar sweet alert
-      dispatch(updateAdmin(formData, adminID, clubName));
-      setCurrentPasswordInput("");
-      setNewPassword("");
-      setConfirmNewPassword("");
-      setCurrentPasswordError("");
-      setNewPasswordError("");
-      setConfirmNewPasswordError("");
+      Swal.fire({
+        title: "¿Estás seguro?",
+        text: "Estás a punto de cambiar tu contraseña",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Sí, cambiar contraseña",
+        cancelButtonText: "No, cancelar",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          dispatch(updateAdmin(formData, adminID, clubName));
+          Swal.fire(
+            "¡Cambiado!",
+            "Has cambiado tu contraseña correctamente.",
+            "success"
+          );
+          setCurrentPasswordInput("");
+          setNewPassword("");
+          setConfirmNewPassword("");
+          setCurrentPasswordError("");
+          setNewPasswordError("");
+          setConfirmNewPasswordError("");
+        }
+      });
     }
   };
-
   console.log("id", adminID);
   console.log("nueva contraseña", newPassword);
 
   return (
-    <div className={style.formContainer}>
-      <form onSubmit={handleSubmit} className={style.form}>
+    <div className={style.changePassword}>
+      <h2>Cambiar Contraseña</h2>
+
+      <form onSubmit={handleSubmit} className={style.FormPostAdminSA}>
         <label htmlFor="currentPassword">Contraseña Actual:</label>
         <div className={style.passwordInput}>
           <input
@@ -124,7 +167,7 @@ function FormUpdatePasswordAdmin() {
           />
         </div>
         {currentPasswordError && (
-          <div className={style.error}>{currentPasswordError}</div>
+          <span className={style.error}>{currentPasswordError}</span>
         )}
 
         <label htmlFor="newPassword">Nueva Contraseña:</label>
@@ -144,7 +187,7 @@ function FormUpdatePasswordAdmin() {
           />
         </div>
         {newPasswordError && (
-          <div className={style.error}>{newPasswordError}</div>
+          <span className={style.error}>{newPasswordError}</span>
         )}
 
         <label htmlFor="confirmNewPassword">Confirmar Nueva Contraseña:</label>
@@ -166,12 +209,12 @@ function FormUpdatePasswordAdmin() {
           />
         </div>
         {confirmNewPasswordError && (
-          <div className={style.error}>{confirmNewPasswordError}</div>
+          <span className={style.error}>{confirmNewPasswordError}</span>
         )}
 
         <button
           type="submit"
-          className={style.button}
+          className={style.btnForms}
           disabled={
             currentPasswordError || newPasswordError || confirmNewPasswordError
           }
