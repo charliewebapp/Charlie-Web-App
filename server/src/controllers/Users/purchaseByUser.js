7dc581fd11df58d90baf85d482d2a7944428ce5b
Santino <schielsantino@gmail.com>
2024-02-23 10:19:38 -0300

    MOD purchaseByUser

--- server/src/controllers/Users/purchaseByUser.js
+++ server/src/controllers/Users/purchaseByUser.js
@@ -1,27 +1,27 @@
 
-const { User } = require("../../db");
-const { QrCode } = require("../../db");
-const { Client } = require("../../db");
+const { User, Purchase, Client } = require("../../db");
 
 
 const purchaseByUser = async (req, res) => {
   try {
     
     const {idUser, idClient } = req.body; 
+
     const validUsers = await User.findOne({
       where: {
         id: idUser,
       },
     })  
+
     const validClient = await Client.findOne({
       where: {
         id: idClient,
       },
     })
-    console.log(validClient)
+    
     if (!validUsers || !validClient) return res.status(400).json({ error: "Error en información suministrada" });
    
-    const QrCodesByUser = await QrCode.findAll({
+    const QrCodesByUser = await Purchase.findAll({
       where: {
         UserId: idUser, // Asume que hay una columna userId en tu modelo QrCode
         ClientId:idClient
