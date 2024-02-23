99b9c5e419d1e1e9e725649e5c1dc14adf7bf063
Luciano Moren <lucianomorenandre03@gmail.com>
2024-02-23 13:34:45 -0300

    login sa y a responsive

--- client/src/views/SuperAdmin/LoginSuperA/validation.js
+++ client/src/views/SuperAdmin/LoginSuperA/validation.js
@@ -1,20 +1,20 @@
 export default (data) => {
-    let errors = {}
+  let errors = {};
 
-    if (!/\S+@\S+\.\S+/.test(data.email)) {
-        errors.e1 = 'Esto no es un email'
-    }
-    if (!data.email) {
-        errors.e2 = 'Ingresar email'
-    }
-    if (data.email.length > 35) {
-        errors.e3 = "Excede limite"
-    }
-    if (!/.*\d.*/.test(data.password)) {
-        errors.p1 = 'Al menos incluir un numero'
-    }
-    if (data.password.length > 10) {
-        errors.p2 = 'Incorrect Password Length'
-    }
-    return errors
-}
+  if (!/\S+@\S+\.\S+/.test(data.email)) {
+    errors.e1 = "Esto no es un email";
+  }
+  if (!data.email) {
+    errors.e2 = "Ingresar email";
+  }
+  if (data.email.length > 35) {
+    errors.e3 = "Excede limite";
+  }
+  if (!/.*\d.*/.test(data.password)) {
+    errors.p1 = "Al menos incluir un numero";
+  }
+  if (data.password.length > 10) {
+    errors.p2 = "Debe contener menos de 10 caracteres";
+  }
+  return errors;
+};
