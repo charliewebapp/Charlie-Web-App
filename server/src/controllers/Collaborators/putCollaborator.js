400c06aed03ebfc5aec6bb3b14f6d424c73d471e
Nicolas <comercialfecor@gmail.com>
2024-02-26 10:19:59 -0500

    activareinactivarcolab

--- server/src/controllers/Collaborators/putCollaborator.js
+++ server/src/controllers/Collaborators/putCollaborator.js
@@ -1,28 +1,24 @@
 const { Client, Collaborator } = require("../../db");
 
 const putCollaborators = async (client, user, newData) => {
-  console.log(client, user, newData)
+  console.log(client, user, newData);
   const clientSearched = await Client.findOne({ where: { name: client } });
   if (!clientSearched) throw new Error("El cliente no existe.");
   const clientId = clientSearched.dataValues.id;
 
-  const updatedProduct = await Collaborator.update(newData, {
+  const updatedCollaborator = await Collaborator.findOne({
     where: {
       ClientId: clientId,
-      name: user,
     },
   });
 
-  // const updatededProduct = await Collaborator.findOne({
-  //   where: {
-  //     ClientId: clientId,
-  //     id: user,
-  //   },
-  // });
+  await Collaborator.update(newData, {
+    where: {
+      ClientId: clientId,
+    },
+  });
 
-
-  return updatedProduct
-
+  return updatedCollaborator;
 };
 
 module.exports = putCollaborators;
